ca0390e0b0255f336bdbf6fb1d80adc4e8a52aa0
Dmitry Khodyrev <dooommm@mail.ru>
2023-01-14 15:50:16 +0800

    fix image paths

--- src/utils/getImgUrl.ts
+++ src/utils/getImgUrl.ts
@@ -4,13 +4,13 @@ export function getImgUrl(name: Planet, infoType: InfoType) {
   const planet = name.toLowerCase();
   switch (infoType) {
     case 'overview':
-      return [`public/images/planet-${planet}.svg`];
+      return [`/images/planet-${planet}.svg`];
     case 'internal':
-      return [`public/images/planet-${planet}-internal.svg`];
+      return [`/images/planet-${planet}-internal.svg`];
     case 'surface':
       return [
-        `/public/images/planet-${planet}.svg`,
-        `/public/images/planet-${planet}-geology.png`,
+        `/images/planet-${planet}.svg`,
+        `/images/planet-${planet}-geology.png`,
       ];
   }
 }
